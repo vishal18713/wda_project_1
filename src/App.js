67b71d428d8d6d496c83cbbfa8dfd397c6fe5296
vishal18713 <lcs2023052@iiitl.ac.in>
2024-04-15 20:40:01 +0530

    Update CSS and App.js to remove commented out code and add AboutUs page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom'; // Update import
+import { Routes, Route } from 'react-router-dom'; 
 import './App.css';
 import Home from './components/pages/Home';
-import Navbar from './components/Navbar'; // Import Navbar
+import AboutUs from './components/pages/AboutUs';
+import Navbar from './components/Navbar'; 
 
 function App() {
   return (
@@ -10,11 +11,11 @@ function App() {
       <Navbar /> {/* Move Navbar outside of Routes */}
       <Routes>
         <Route path='/' element={<Home />} /> {/* Update Route syntax */}
-        {/* 
-        <Route path='/services' element={<Services />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        */}
+        
+        <Route path='/About_us' element={<AboutUs/>} />
+        {/* <Route path='/products' element={<Products />} /> */}
+        {/* <Route path='/sign-up' element={<SignUp />} /> */}
+       
       </Routes>
     </>
   );
---
+++ src/components/HeroSection1.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection1.css';
+
+export const HeroSection1 = () => {
+  return (
+    <div className='hero-container'>
+      
+    <h1>Mental Health</h1>
+    <p>At our website, we provide resources, support, and information to help you navigate your mental health journey.</p>
+    
+  </div>
+  )
+}
+
+export default HeroSection1
